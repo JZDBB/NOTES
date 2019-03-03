6b1afd4d4736e27e288a4f4eca51a3016719aee0
XuYining <oxuyining@gmail.com>
2019-03-03 16:42:57 +0800

    adagrad and rmsprop
    Signed-off-by: XuYining <oxuyining@gmail.com>

--- 3.Methods/1.Loss/basis.py
+++ 3.Methods/1.Loss/basis.py
@@ -2,6 +2,8 @@
 import tensorflow
 import matplotlib.pyplot as plt
 import numpy as np
+import math
+
 eta = 0.1
 eta2 = 0.4
 
@@ -61,6 +63,32 @@ def f_2d2(x1, x2):
 def gd_2d2(x1, x2, s1, s2):
     return (x1 - eta2 * 0.2 * x1, x2 - eta2 * 4 * x2, 0, 0)
 
+eta3, gamma = 0.4, 0.5
+def momentum_2d(x1, x2, v1, v2):
+    v1 = gamma * v1 + eta3 * 0.2 * x1
+    v2 = gamma * v2 + eta3 * 4 * x2
+    return x1 - v1, x2 - v2, v1, v2
+
+eta4 = 2
+def adagrad_2d(x1, x2, s1, s2):
+    g1, g2, eps = 0.2 * x1, 4 * x2, 1e-6  # 前两项为自变量梯度。
+    s1 += g1 ** 2
+    s2 += g2 ** 2
+    x1 -= eta4 / math.sqrt(s1 + eps) * g1
+    x2 -= eta4 / math.sqrt(s2 + eps) * g2
+    return x1, x2, s1, s2
+
+eta5, gamma2 = 0.4, 0.9
+def rmsprop_2d(x1, x2, s1, s2):
+    g1, g2, eps = 0.2 * x1, 4 * x2, 1e-6
+    s1 = gamma2 * s1 + (1 - gamma2) * g1 ** 2
+    s2 = gamma2 * s2 + (1 - gamma2) * g2 ** 2
+    x1 -= eta5 / math.sqrt(s1 + eps) * g1
+    x2 -= eta5 / math.sqrt(s2 + eps) * g2
+    return x1, x2, s1, s2
+
+
+
 def network(learning_rate, batch_size):
     pass
 
@@ -76,4 +104,11 @@ if __name__ == '__main__':
     # show_trace_2d(f_2d, train_2d(sgd_2d))
 
     # momentum
-    show_trace_2d(f_2d2, train_2d(gd_2d2))
+    # show_trace_2d(f_2d2, train_2d(gd_2d2))
+    # show_trace_2d(f_2d, train_2d(momentum_2d))
+
+    # adagrad
+    # show_trace_2d(f_2d2, train_2d(adagrad_2d))
+
+    # rmsprop
+    show_trace_2d(f_2d2, train_2d(rmsprop_2d))
